813443ebd49216852aadebf81f2e8c2887268fca
shsato3199 <130293292+shsato3199@users.noreply.github.com>
2026-09-24 14:11:41 +0900

    Initial commit

---
+++ src/main/java/com/shsato/moneymanagerapi/MoneyManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.shsato.moneymanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shsato/moneymanagerapi/MoneyManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shsato.moneymanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
